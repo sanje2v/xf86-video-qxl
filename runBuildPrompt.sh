1cc998e4a5550231f08edbe4c23f8406231808a5
Sanjeev <swtbase@yahoo.com>
2023-01-13 22:36:50 +1100

    Added Dockerfile to build

--- runBuildPrompt.sh
+++ runBuildPrompt.sh
@@ -2,6 +2,7 @@
 CURRENT_DIR_NAME=${PWD##*/}
 
 docker build --build-arg HOST_UID=$(id -u) --build-arg HOST_NAME=$(whoami) -f Dockerfile -t $CURRENT_DIR_NAME . && \
+mkdir -p ./install \
 docker run --rm -it \
            -v $(pwd):/home/$(whoami)/$CURRENT_DIR_NAME:rw \
            -v $HOME/.ssh:/home/$(whoami)/.ssh:ro \
